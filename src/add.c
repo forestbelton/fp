b5f28c5e0b9f07bbc9b1cae6275654860f9768b6
Forest Belton <fbelton@theore.tc>
2012-02-22 23:59:45 -0500

    Added support for addition when the destination operand is the same as one of the source operands.

--- src/add.c
+++ src/add.c
@@ -37,6 +37,7 @@ void fp_add(fp_t *a, fp_t *b, fp_t *c) {
   int     i;
   uint8_t tmp, carry;
   uint8_t offset[2];
+  fp_t    out;
   
   /* Use the larger of the two exponents and compute the distance between
    * it and a->expt, b->expt. These offsets are used to simulate the adjustment
@@ -44,20 +45,21 @@ void fp_add(fp_t *a, fp_t *b, fp_t *c) {
    * operation. This means that a and b don't have to be clobbered, duplicated
    * on the stack, etc. and generally saves having to iterate through the
    * fractional component of their data. */
-  c->expt   = a->expt > b->expt ? a->expt : b->expt;
-  offset[0] = c->expt - a->expt;
-  offset[1] = c->expt - b->expt;
+  out.expt  = a->expt > b->expt ? a->expt : b->expt;
+  offset[0] = out.expt - a->expt;
+  offset[1] = out.expt - b->expt;
   
   /* Determine sign of result. */
   switch((a->sgn << 1) | b->sgn) {
     /* c <- a + b = a + b    */
     case 0:
-      c->sgn = 0;
+      out.sgn = 0;
     break;
     
     /* c <- a + (-b) = a - b    */
     case 1:
-      fp_sub(a, b, c, offset);
+      fp_sub(a, b, &out, offset);
+      *c = out;
     return;
     
     /* c <- (-a) + b = b - a    */
@@ -65,12 +67,13 @@ void fp_add(fp_t *a, fp_t *b, fp_t *c) {
       tmp       = offset[0];
       offset[0] = offset[1];
       offset[1] = tmp;
-      fp_sub(b, a, c, offset);
+      fp_sub(b, a, &out, offset);
+      *c = out;
     return;
     
     /* c <- (-a) + (-b) = -(a + b) */
     case 3:
-      c->sgn = 1;
+      out.sgn = 1;
     break;  
   }
   
@@ -84,16 +87,16 @@ void fp_add(fp_t *a, fp_t *b, fp_t *c) {
     if(carry)
       tmp -= 10;
     
-    /* Update c with result. */
-    fp_setdigit(c, i, tmp);
+    /* Update out with result. */
+    fp_setdigit(&out, i, tmp);
   }
   
   /* If we still have carry, then this means that we are done performing
    * normalization after done shifting once. Then set the topmost digit to
    * 1, the carry value. */
   if(carry) {
-    fp_rshift(c, 1);
-    fp_setdigit(c, 0, 1);
+    fp_rshift(&out, 1);
+    fp_setdigit(&out, 0, 1);
   }
   
   /* Shift to the left until we have a leading nonzero digit. The value
@@ -102,7 +105,10 @@ void fp_add(fp_t *a, fp_t *b, fp_t *c) {
   else {
     for(i = 0; i < (int)(sizeof a->data * 2); ++i) {
       if(a->data[0] >> 4) break;
-      fp_lshift(c, 1);
+      fp_lshift(&out, 1);
     }
   }
+  
+  /* Update c with the result. */
+  *c = out;
 }
