af11d42be41fd4f743fe851f07d3f8cd8bf36e39
Forest Belton <fbelton@theore.tc>
2012-02-23 12:48:20 -0500

    Removed debug print statements.

--- src/misc.c
+++ src/misc.c
@@ -52,8 +52,6 @@ void fp_fromstr(fp_t *out, const char *str) {
    * space left. */
   i = 0;
   while(*str) {
-    printf("i: %u\n", i);
-
     /* Note if the decimal point has been encountered. */
     if(*str == '.') {
       done = 1;
@@ -68,16 +66,12 @@ void fp_fromstr(fp_t *out, const char *str) {
     
     /* The decimal point has been encountered and the floating-point value has
      * been filled with digits. Any further iteration would be needless. */
-    if(done && i == sizeof out->data * 2) {
-      printf("no more room\n");
+    if(done && i == sizeof out->data * 2)
       break;
-    }
     
     /* Add the digit if there is any room left and continue iteration. */
-    if(i < sizeof out->data * 2) {
-      printf("adding digit %c\n", *str);
+    if(i < sizeof out->data * 2)
       fp_setdigit(out, i++, *str - '0');
-    }
     ++str;
   }
 }
