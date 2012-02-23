1d67555e654a0dddd0948ef97ad92ac4d7f2d140
Forest Belton <fbelton@theore.tc>
2012-02-23 00:08:44 -0500

    Finished initial revision of division routine.

--- src/div.c
+++ src/div.c
@@ -20,6 +20,7 @@
  */
 #include "fp.h"
 #include "util.h"
+#include <stddef.h>
 
 /* This is an INTERNAL function which computes an approximation to the
  * reciprocal. This function will likely change over time, though it need not
@@ -43,18 +44,32 @@ static void fp_recip(fp_t *a, fp_t *b) {
   
 }
 
+static fp_t FP_TWO = {1, 0x80, {2}};
+
 void fp_div(fp_t *a, fp_t *b, fp_t *c) {
   fp_t a1 = *a;
   fp_t b1 = *b;
+  fp_t tmp, out;
+  size_t i;
   
   /* Scale quotient. */
   a1.expt = a->expt - b->expt + 0x80 - 1;
+  b1.expt = 0x7f;
   
   /* Compute guess. */
-  fp_recip(&b1, c);
+  fp_recip(&b1, &out);
   
   /* Do a few steps of Newton-Raphson iteration. */
+  for(i = 0; i < 10; ++i) {
+    fp_mul(&b1, &out, &tmp);
+    tmp.sgn = 1;
+    fp_add(&FP_TWO, &tmp, &out);
+    fp_mul(&out, &tmp, &out);
+  }
   
+  /* Compute quotient based on (hopefully) good reciprocal approximation. */
+  fp_mul(&out, &a1, &out);
+  *c = out;
 }
 
 /*
