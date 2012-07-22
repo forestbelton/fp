27c8418c5a118c1ac5dcd225723120cc56d910ff
Forest Belton <fbelton@theore.tc>
2012-07-22 22:33:47 +0200

    Update utility functions for new format

--- include/util.h
+++ include/util.h
@@ -24,73 +24,31 @@
 /* Extracts a single digit at the index n. If the index is
  * out of bounds, return 0. */
 static inline uint8_t fp_getdigit(fp_t *f, int n) {
-  if(n < 0 || n > (int)(sizeof f->data * 2)) return 0;
-  return (f->data[n / 2] >> (!(n % 2) * 4)) & 0x0f;
+  if(n < 0 || n > (sizeof f->data - 1) * 2)
+    return 0;
+  else
+    return (f->data >> (4 * n)) & 0x0f;
 }
 
 /* Sets a single digit at the index n. */
 static inline void fp_setdigit(fp_t *f, uint8_t n, uint8_t value) {
-  f->data[n / 2] &= 0xf   << (4 * (n % 2));  /* Clear previous digit. */
-  f->data[n / 2] |= value << (4 * !(n % 2)); /* Set new value. */
+  f->data &= ~(0xf << (4 * n));
+  f->data |= (value & 0xf) << (4 * n);
 }
 
 /* Shifts the floating point value n digits to the right, automatically
  * adjusting the exponent. If the exponent were not adjusted, this operation
  * would be equivalent to dividing by 10^n. */
 static inline void fp_rshift(fp_t *f, uint8_t n) {
-  unsigned i   = 0;
-  uint8_t last = 0;
-  uint8_t tmp;
-  
-  /* Copy data in bytes as much as we can. This saves a great deal of
-   * complexity, since separate digits don't need to be extracted and then
-   * shifted. */
-  while(n > 1) {
-    last = 0;
-    for(i = 0; i < sizeof f->data; ++i) {
-      tmp        = f->data[i];
-      f->data[i] = last;
-      last       = tmp;
-    }
-    n -= 2;
-    f->expt += 2;
-  }
-  
-  /* If there is a single shift left, copy it nibble by nibble. */
-  if(n != 0) { 
-    last = 0;
-    for(i = 0; i < sizeof f->data; ++i) { 
-      tmp        = (f->data[i] & 0x0f) << 4;
-      f->data[i] = last | (f->data[i] >> 4);
-      last       = tmp;
-    }
-    f->expt++;
-  }
+  f->data >>= n;
+  f->expt  += n;
 }
 
 static inline void fp_lshift(fp_t *f, uint8_t n) {
-  unsigned i = 0;
-  
-  /* Copy data in bytes as much as we can. This saves a great deal of
-   * complexity, since separate digits don't need to be extracted and then
-   * shifted. */
-  while(n > 1) {
-    for(i = 0; i < sizeof f->data - 1; ++i)
-      f->data[i] = f->data[i + 1];
-    f->data[sizeof f->data - 1] = 0;
-    n -= 2;
-    f->expt -= 2;
-  }
-  
-  /* If there is a single shift left, copy it nibble by nibble. */
-  if(n != 0) { 
-    for(i = 0; i < sizeof f->data - 1; ++i) {
-      f->data[i] <<= 4;
-      f->data[i]  |= f->data[i + 1] >> 4;
-    }
-    f->data[sizeof f->data - 1] <<= 4;
-    f->expt--;
-  }
+  f->data <<= n;
+  f->data  &= ~(0xffULL << 56);
+  f->expt  -= n;
 }
 
 #endif
+
